ce01c71dfd0680bce41a195ea4fe0d1b2ca0c0e2
David Morgenstern <DavidtheConfident@gmail.com>
2022-11-29 20:50:42 -0600

    Creates Player Class

--- client.py
+++ client.py
@@ -7,6 +7,36 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 3
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+
+
 def redrawWindow():
     win.fill((255,255,255))
     pygame.display.update()
@@ -19,3 +49,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+
+        redrawWindow()
+
+
