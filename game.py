30f8aac793df4e312dcf848984010f1a50b9d6e1
David Morgenstern <DavidtheConfident@gmail.com>
2022-12-04 16:49:47 -0600

    Creates Game file

---
+++ game.py
@@ -0,0 +1,7 @@
+class Game:
+    def __init__(self, id):
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+        self.id = id
+        self.moves = [None, None]
--- network.py
+++ network.py
@@ -15,14 +15,14 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e: 
             print(e)
 
